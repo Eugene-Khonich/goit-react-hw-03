5ccdc44a8847c3d544281f266399443ac2d945a9
Eugene-Khonich <ehonich@gmail.com>
2024-10-06 21:46:15 +0300

    1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,18 @@
+import css from './Contact.module.css';
+import { MdPerson } from 'react-icons/md';
+import { MdPhone } from 'react-icons/md';
+
 const Contact = ({ name, number }) => {
   return (
-    <div>
-      <h3>{name}</h3>
-      <p>{number}</p>
-      <button>Delete</button>
+    <div className={css.card}>
+      <h3 className={css.name}>
+        <MdPerson />
+        {name}
+      </h3>
+      <p className={css.number}>
+        <MdPhone />
+        {number}
+      </p>
     </div>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,14 @@
+import css from './ContactLIst.module.css';
 import Contact from '../Contact/Contact';
+
 const ContactList = ({ contacts }) => {
   return (
-    <ul>
+    <ul className={css.list}>
       {contacts.map(item => {
         return (
-          <li key={item.id}>
+          <li key={item.id} className={css.card}>
             <Contact name={item.name} number={item.number} />
+            <button className={css.btn}>Delete</button>
           </li>
         );
       })}
