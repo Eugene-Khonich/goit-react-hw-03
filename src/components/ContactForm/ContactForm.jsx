cef5a346a38243d4034588926ac5f74a508753c7
Eugene-Khonich <ehonich@gmail.com>
2024-10-06 18:55:56 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,27 @@
+import { Formik, Field, ErrorMessage, Form } from 'formik';
+
+const INITIAL_VALUES = {
+  name: '',
+  phone: '',
+};
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  return (
+    <Formik initialValues={INITIAL_VALUES}>
+      <Form>
+        <label htmlFor="text">
+          <span>Name</span>
+          <Field type="text" name="name" />
+          <ErrorMessage name="name" component="span" />
+        </label>
+        <label htmlFor="phone">
+          <span>Number</span>
+          <Field type="text" name="phone" />
+          <ErrorMessage name="phone" component="span" />
+        </label>
+        <button>Add contact</button>
+      </Form>
+    </Formik>
+  );
 };
 
 export default ContactForm;
